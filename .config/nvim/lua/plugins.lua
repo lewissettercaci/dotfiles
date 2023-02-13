70d810b6559b0fff05823fc5724a6648b3c5beff
Lewis Setter <lewis.setter@caci.com>
2023-02-13 09:25:14 -0500

    update dap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -110,7 +110,7 @@ local plugins = {
         'mfussenegger/nvim-dap',
         config = function()
             local dap = require('dap')
-            dap.adapters.lldb = { type = 'executable', command = '/usr/bin/lldb-vscode-14', name = 'lldb' }
+            dap.adapters.lldb = { type = 'executable', command = 'lldb-vscode', name = 'lldb' }
             dap.configurations.cpp = {
                 {
                     name = 'Launch',
@@ -122,29 +122,12 @@ local plugins = {
                     cwd = '${workspaceFolder}',
                     stopOnEntry = false,
                     args = {}
-
-                    -- 💀
-                    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-                    --
-                    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-                    --
-                    -- Otherwise you might get the following error:
-                    --
-                    --    Error on launch: Failed to attach to the target process
-                    --
-                    -- But you should be aware of the implications:
-                    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-                    -- runInTerminal = false,
                 }
             }
-
-            -- If you want to use this for Rust and C, add something like this:
-
-            -- dap.configurations.c = dap.configurations.cpp
-            -- dap.configurations.rust = dap.configurations.cpp
         end
     },
-    { 'rcarriga/nvim-dap-ui', config = function() require'dapui'.setup() end }
+    { 'rcarriga/nvim-dap-ui', config = function() require'dapui'.setup() end },
+    { 'theHamsta/nvim-dap-virtual-text', config = function() require'nvim-dap-virtual-text'.setup() end }
 }
 
 require'packer'.startup({
